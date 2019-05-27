289f0b2b2b4b6aca2efeb8f5cce4b0c4e231f5b7
caglaror <caglaror@gmail.com>
2019-05-27 00:30:29 -0700

    Refactored

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-
-module.exports = {
-    entry: './foo.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'foo.bundle.js'
-    }
-};
